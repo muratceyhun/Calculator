94b94b518ffeeec43750c26acfbc539418086fb7
Murat Ceyhun KORPEOGLU <muratceyhunkorpeoglu@gmail.com>
2023-01-03 21:01:58 +0300

    View stuff is done.

--- Calculator/ViewController.swift
+++ Calculator/ViewController.swift
@@ -8,12 +8,91 @@
 import UIKit
 
 class ViewController: UIViewController {
+    
+    @IBOutlet var holder: UIView!
+    
+    private var resultLabel: UILabel = {
+        let label = UILabel()
+        label.text = "0"
+        label.textColor = .white
+        label.textAlignment = .right
+        label.font = UIFont(name: "Arial", size: 32)
+        return label
+    }()
 
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view.
     }
+    
+    
+    override func viewDidLayoutSubviews() {
+        super.viewDidLayoutSubviews()
+        
+        setupNumberPad()
+        
+        
+        
+    }
+    private func setupNumberPad() {
+        let buttonSize: CGFloat = view.frame.width/4
+        let zeroButton = UIButton(frame: CGRect(x: 0, y: holder.frame.height-buttonSize, width: buttonSize*3, height: buttonSize))
+        zeroButton.setTitleColor(.black, for: .normal)
+        zeroButton.backgroundColor = .white
+        zeroButton.setTitle("0", for: .normal)
+        holder.addSubview(zeroButton)
+        
+        for x in 0..<3 {
+            let button1 = UIButton(frame: CGRect(x: buttonSize * CGFloat(x), y: holder.frame.height-(buttonSize*2), width: buttonSize, height: buttonSize))
+            button1.setTitleColor(.black, for: .normal)
+            button1.backgroundColor = .white
+            button1.setTitle("\(x+1)", for: .normal)
+            holder.addSubview(button1)
+        }
+        
+        for x in 0..<3 {
+            let button2 = UIButton(frame: CGRect(x: buttonSize * CGFloat(x), y: holder.frame.height-(buttonSize*3), width: buttonSize, height: buttonSize))
+            button2.setTitleColor(.black, for: .normal)
+            button2.backgroundColor = .white
+            button2.setTitle("\(x+4)", for: .normal)
+            holder.addSubview(button2)
+        }
+        
+        for x in 0..<3 {
+            let button3 = UIButton(frame: CGRect(x: buttonSize * CGFloat(x), y: holder.frame.height-(buttonSize*4), width: buttonSize, height: buttonSize))
+            button3.setTitleColor(.black, for: .normal)
+            button3.backgroundColor = .white
+            button3.setTitle("\(x+7)", for: .normal)
+            holder.addSubview(button3)
+        }
+        
+        let clearButton = UIButton(frame: CGRect(x: 0, y: holder.frame.height-(buttonSize*5), width: view.frame.width, height: buttonSize))
+        clearButton.setTitleColor(.black, for: .normal)
+        clearButton.backgroundColor = .white
+        clearButton.setTitle("AC", for: .normal)
+        holder.addSubview(clearButton)
+        
+        let operations = ["+","-","x","/"]
+        
+        for x in 0..<4 {
+            let button4 = UIButton(frame: CGRect(x: buttonSize * 3, y: holder.frame.height-(buttonSize * CGFloat(x+1)), width: buttonSize, height: buttonSize))
+            button4.setTitleColor(.white, for: .normal)
+            button4.backgroundColor = .orange
+            button4.setTitle(operations[x], for: .normal)
+            holder.addSubview(button4)
+            
+            
+        
+        
+        
+        
+    }
+        
+        resultLabel.frame = CGRect(x: 0, y: clearButton.frame.origin.y - 75, width: view.frame.width - 45, height: 100)
+        holder.addSubview(resultLabel)
+
 
 
 }
 
+}
