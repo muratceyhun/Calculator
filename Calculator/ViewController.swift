66b2dc57cc33842406301a3eec96c3603da9822e
Murat Ceyhun KORPEOGLU <muratceyhunkorpeoglu@gmail.com>
2023-01-03 23:38:35 +0300

    The project has been completed.

--- Calculator/ViewController.swift
+++ Calculator/ViewController.swift
@@ -11,6 +11,15 @@ class ViewController: UIViewController {
     
     @IBOutlet var holder: UIView!
     
+    
+    var firstNumber = 0
+    var resultNumber = 0
+    var currentOperation : Operation?
+    
+    enum Operation {
+        case add, subtract, multiply, divide
+    }
+    
     private var resultLabel: UILabel = {
         let label = UILabel()
         label.text = "0"
@@ -33,6 +42,7 @@ class ViewController: UIViewController {
         
         
         
+        
     }
     private func setupNumberPad() {
         let buttonSize: CGFloat = view.frame.width/4
@@ -40,14 +50,19 @@ class ViewController: UIViewController {
         zeroButton.setTitleColor(.black, for: .normal)
         zeroButton.backgroundColor = .white
         zeroButton.setTitle("0", for: .normal)
+        zeroButton.tag = 1
         holder.addSubview(zeroButton)
+        zeroButton.addTarget(self, action: #selector(zeroClicked), for: .touchUpInside)
         
         for x in 0..<3 {
             let button1 = UIButton(frame: CGRect(x: buttonSize * CGFloat(x), y: holder.frame.height-(buttonSize*2), width: buttonSize, height: buttonSize))
             button1.setTitleColor(.black, for: .normal)
             button1.backgroundColor = .white
             button1.setTitle("\(x+1)", for: .normal)
+            button1.tag = x+2
             holder.addSubview(button1)
+            button1.addTarget(self, action: #selector(numberPressed(_:)), for: .touchUpInside)
+
         }
         
         for x in 0..<3 {
@@ -55,7 +70,10 @@ class ViewController: UIViewController {
             button2.setTitleColor(.black, for: .normal)
             button2.backgroundColor = .white
             button2.setTitle("\(x+4)", for: .normal)
+            button2.tag = x+5
             holder.addSubview(button2)
+            button2.addTarget(self, action: #selector(numberPressed(_:)), for: .touchUpInside)
+
         }
         
         for x in 0..<3 {
@@ -63,23 +81,27 @@ class ViewController: UIViewController {
             button3.setTitleColor(.black, for: .normal)
             button3.backgroundColor = .white
             button3.setTitle("\(x+7)", for: .normal)
+            button3.tag = x+8
             holder.addSubview(button3)
+            button3.addTarget(self, action: #selector(numberPressed(_:)), for: .touchUpInside)
         }
         
-        let clearButton = UIButton(frame: CGRect(x: 0, y: holder.frame.height-(buttonSize*5), width: view.frame.width, height: buttonSize))
+        let clearButton = UIButton(frame: CGRect(x: 0, y: holder.frame.height-(buttonSize*5), width: view.frame.width - buttonSize, height: buttonSize))
         clearButton.setTitleColor(.black, for: .normal)
         clearButton.backgroundColor = .white
         clearButton.setTitle("AC", for: .normal)
         holder.addSubview(clearButton)
         
-        let operations = ["+","-","x","/"]
+        let operations = ["=","+","-","x","/"]
         
-        for x in 0..<4 {
+        for x in 0..<5 {
             let button4 = UIButton(frame: CGRect(x: buttonSize * 3, y: holder.frame.height-(buttonSize * CGFloat(x+1)), width: buttonSize, height: buttonSize))
             button4.setTitleColor(.white, for: .normal)
             button4.backgroundColor = .orange
             button4.setTitle(operations[x], for: .normal)
             holder.addSubview(button4)
+            button4.tag = x+1
+            button4.addTarget(self, action: #selector(operationPressed), for: .touchUpInside)
             
             
         
@@ -90,9 +112,96 @@ class ViewController: UIViewController {
         
         resultLabel.frame = CGRect(x: 0, y: clearButton.frame.origin.y - 75, width: view.frame.width - 45, height: 100)
         holder.addSubview(resultLabel)
-
+        
+        
+        //MARK: -Actions
+        
+        
+        clearButton.addTarget(self, action: #selector(clearResult), for: .touchUpInside)
+        
 
 
 }
+    
+    @objc func zeroClicked() {
+        
+        if resultLabel.text != "0" {
+            if let text = resultLabel.text {
+                resultLabel.text = "\(text)\(0)"
+            }
+        }
+        
+    }
+    
+    @objc func clearResult() {
+        resultLabel.text = "0"
+        currentOperation = nil
+        firstNumber = 0
+    }
+    
+    
+    @objc func numberPressed(_ sender: UIButton) {
+        let tag = sender.tag - 1
+        if resultLabel.text == "0" {
+            resultLabel.text = "\(tag)"
+        } else if let text = resultLabel.text {
+            resultLabel.text = "\(text)\(tag)"
+        }
+    }
+    
+    @objc func operationPressed(_ sender: UIButton) {
+        
+        let tag = sender.tag
+        
+        if let text = resultLabel.text, let value = Int(text), firstNumber == 0 {
+            firstNumber = value
+            resultLabel.text = ""
+
+        }
+        
+        if tag == 1 {
+            if let operation = currentOperation {
+                var secondNumber = 0
+                if let text = resultLabel.text, let value = Int(text) {
+                    secondNumber = value
+                }
+                 
+                switch operation {
+                case .add:
+                    let result = firstNumber + secondNumber
+                    resultLabel.text = "\(result)"
+                    break
+                case .subtract:
+                    let result = firstNumber - secondNumber
+                    resultLabel.text = "\(result)"
+                    break
+                case .multiply:
+                    let result = firstNumber * secondNumber
+                    resultLabel.text = "\(result)"
+                    break
+                case .divide:
+                    let result = firstNumber / secondNumber
+                    resultLabel.text = "\(result)"
+                    break
+                }
+            }
+        }
+        else if tag == 2 {
+            currentOperation = .add
+
+        }
+        else if tag == 3 {
+            currentOperation = .subtract
+        }
+        else if tag == 4 {
+            currentOperation = .multiply
+        }
+        else if tag == 5 {
+            currentOperation = .divide
+        }
+
+
+       
+    }
 
 }
